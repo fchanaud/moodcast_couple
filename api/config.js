da20283617b6e7302d5a1f9d393025e41abc6360
fchanaud <franklin.dechanaud@gmail.com>
2025-05-30 18:18:17 +0100

    🔧 Fix Vercel deployment failure - Simplify config and use CommonJS

--- api/config.js
+++ api/config.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   // Définir les headers CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
@@ -20,4 +20,4 @@ export default function handler(req, res) {
     pushoverApiToken: process.env.PUSHOVER_API_TOKEN || '',
     pushoverUserKey: process.env.PUSHOVER_USER_KEY || ''
   });
-} 
+}; 
